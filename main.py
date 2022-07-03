f1d4cfdd65857c20a84496a4e7debe33cc5789e9
Giuseppe Steduto <peppesteduto@gmail.com>
2022-07-03 18:38:31 +0200

    The script now logs to a file

--- main.py
+++ main.py
@@ -1,58 +1,81 @@
-"""
-Author: Giuseppe Steduto
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import title_contains
+from datetime import datetime
 
-This script is intended to be constantly run to check the availability of concert tickets on FanSale (by Ticketone)
-that meet the specified criteria
-"""
-import requests as requests
-import selenium as s
-import bs4
 
-def check_url(u):
-    if u == "":
-        return False
-    return True
-
-def print_cookies(cj):
-    for cookie in cj:
-        print(cookie.name, end=" - ")
-    print("\n")
+def scrivi_log(str):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    f = open("/fansale_tracker/trovati.txt", "a")
+    f.write(current_time + " ||| " + str + "\n")
+    f.close()
 
 
 URL = "https://www.fansale.it/fansale/tickets/pop-amp-rock/pinguini-tattici-nucleari/558153/13359444"
+opt = Options()
+opt.headless = True
+browser = webdriver.Firefox(options=opt)
+wait = WebDriverWait(browser, 10)
+tickets_description = []
+parterre = 0 #Numero di posti parterre
+browser.get(URL)
+wait.until(title_contains("I migliori biglietti per"))
+if "Sfortunatamente" not in browser.page_source:
+    tickets = browser.find_elements(By.CLASS_NAME, "OfferEntry-Top")
+else:
+    tickets = []
+for t in tickets:
+    if t.text == "":
+        continue
+    if t.text.find("Parterre") == -1:
+        posto = t.text[(t.text.find("Posto ") + len("Posto ")):][:2]
+        fila = t.text[(t.text.find("Fila ") + len("Fila ")):][:2]
+        settore = t.text[(t.text.find("Settore ") + len("Settore ")):][:3]
+        tickets_description.append({
+            "posto": int(posto),
+            "fila": int(fila),
+            "settore": settore
+        })
+    else:
+        parterre += 1
 
-# Input and check URL
-URL_INPUT = ""
-while not check_url(URL_INPUT):
-    URL_INPUT = input("Insert the complete Fansale input of the event you want to bui tickets for: ")
+# Adesso trova se esistono almeno due posti nello stesso settore
+for t1 in tickets_description:
+    same_sector = [t1]
+    for t2 in tickets_description:
+        if t1 != t2:
+            if t1["settore"] == t2["settore"]:
+                same_sector.append(t2)
+    for s in same_sector:
+        tickets_description.remove(s)
 
+    # Per tutti quelli nello stesso settore, vedi se ci sono almeno due nella stessa fila:
+    for ts in same_sector:
+        fila = [ts["posto"]]
+        el = [ts]
+        for t2 in same_sector:
+            if ts != t2:
+                if ts["fila"] == t2["fila"]:
+                    fila.append(t2["posto"])
+                    el.append(t2)
+        for e in el:
+            same_sector.remove(e)
 
-# Extract the ticket list from the website
-headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"} #Change UA
-response_challenge_validator = requests.get(URL, headers=headers)
-cookies = response_challenge_validator.cookies
-print("Prima richiesta: ", end="")
-print_cookies(cookies)
+        # Vedi se esistono due posti contigui
+        fila.sort()
+        for i in range(0, len(fila) - 1):
+            if abs(fila[i] - fila[i + 1]) == 1:
+                scrivi_log("Ci sono due posti vicini! Settore " + t1["settore"] +  " | Fila " + str(t1["fila"]) +  " | Posti " + str(fila[i]) +  " e " +  str(fila[i + 1]))
 
-res_challenge_validator3 = requests.get("https://www.fansale.it/_sec/cp_challenge/ak-challenge-3-6.htm", headers=headers, cookies=cookies.get_dict())
-cookies.update(res_challenge_validator3.cookies)
-print("Dopo aver fatto richiesta a ak-challenge: ", end="")
-print_cookies(cookies)
+browser.close()
 
-res_challenge_validator4_verify = requests.get("https://www.fansale.it/_sec/cp_challenge/verify", headers=headers, cookies=cookies.get_dict())
-# cookies.update(res_challenge_validator4_verify.cookies)
-print("Dopo aver fatto richiesta a verify: ", end="")
-print_cookies(cookies)
-
-# TODO aggiornare i cookie con quelli che vengono richiamati dall'XHR con il JS che viene inviato in ak-challenge
-
-res = requests.get(URL, headers=headers, cookies=cookies.get_dict())
-print("Inviati per la richiesta finale: ", end="")
-print_cookies(cookies)
-print("************")
-print(res.content)
-
-"""
-soup = bs4.BeautifulSoup(response.content, 'html.parser')
-rev_div = soup.findAll("div", attrs={"class", "EventEntry"})
-"""
+if parterre != 0:
+    scrivi_log("Posti parterre: " + str(parterre))
+uffi = open("/fansale_tracker/log.txt", "a")
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+uffi.write(current_time + "\n")
+uffi.close()
