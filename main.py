23f4dff70783811c3dea3c27ccd348c7f20b2360
Giuseppe Steduto <peppesteduto@gmail.com>
2022-07-04 16:28:03 +0200

    Improved functionality and modularity. Added support for .env file.

--- main.py
+++ main.py
@@ -1,81 +1,45 @@
+import requests
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import title_contains
 from datetime import datetime
+import os
+from dotenv import load_dotenv
 
 
-def scrivi_log(str):
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    f = open("/fansale_tracker/trovati.txt", "a")
-    f.write(current_time + " ||| " + str + "\n")
-    f.close()
+def send_notification(str, chat_id):
+    """ Send a Telegram message to a specified user """
+    api_token = os.getenv("API_TOKEN")
+    # Send message to my chat on Telegram
+    requests.get("https://api.telegram.org/bot" + api_token + "/sendMessage?chat_id=" + chat_id + "&text=" + str)
 
 
-URL = "https://www.fansale.it/fansale/tickets/pop-amp-rock/pinguini-tattici-nucleari/558153/13359444"
+# Configuration part
+load_dotenv()   # Load env variables from .env file
+minimum_seats = int(os.getenv("MINIMUM_SEATS"))  # The minimum number of seats over which you want to be notified
+telegram_chat_id = os.getenv("CHAT_ID")
+URL = os.getenv("URL")  # The URL of the event on fansale
 opt = Options()
-opt.headless = True
+opt.headless = True     # Run the browser in headless mode so that it can be run by a cronjob
 browser = webdriver.Firefox(options=opt)
 wait = WebDriverWait(browser, 10)
-tickets_description = []
-parterre = 0 #Numero di posti parterre
+
+# Emulate the browser and request the page
 browser.get(URL)
-wait.until(title_contains("I migliori biglietti per"))
-if "Sfortunatamente" not in browser.page_source:
-    tickets = browser.find_elements(By.CLASS_NAME, "OfferEntry-Top")
-else:
-    tickets = []
+wait.until(title_contains("I migliori biglietti per"))  # Wait after challenge validation, when page is fully loaded
+tickets = browser.find_elements(By.CLASS_NAME, "NumberOfTicketsInOffer")
 for t in tickets:
-    if t.text == "":
-        continue
-    if t.text.find("Parterre") == -1:
-        posto = t.text[(t.text.find("Posto ") + len("Posto ")):][:2]
-        fila = t.text[(t.text.find("Fila ") + len("Fila ")):][:2]
-        settore = t.text[(t.text.find("Settore ") + len("Settore ")):][:3]
-        tickets_description.append({
-            "posto": int(posto),
-            "fila": int(fila),
-            "settore": settore
-        })
-    else:
-        parterre += 1
-
-# Adesso trova se esistono almeno due posti nello stesso settore
-for t1 in tickets_description:
-    same_sector = [t1]
-    for t2 in tickets_description:
-        if t1 != t2:
-            if t1["settore"] == t2["settore"]:
-                same_sector.append(t2)
-    for s in same_sector:
-        tickets_description.remove(s)
-
-    # Per tutti quelli nello stesso settore, vedi se ci sono almeno due nella stessa fila:
-    for ts in same_sector:
-        fila = [ts["posto"]]
-        el = [ts]
-        for t2 in same_sector:
-            if ts != t2:
-                if ts["fila"] == t2["fila"]:
-                    fila.append(t2["posto"])
-                    el.append(t2)
-        for e in el:
-            same_sector.remove(e)
-
-        # Vedi se esistono due posti contigui
-        fila.sort()
-        for i in range(0, len(fila) - 1):
-            if abs(fila[i] - fila[i + 1]) == 1:
-                scrivi_log("Ci sono due posti vicini! Settore " + t1["settore"] +  " | Fila " + str(t1["fila"]) +  " | Posti " + str(fila[i]) +  " e " +  str(fila[i + 1]))
-
+    text = t.get_attribute('innerText')
+    if t.is_displayed():
+        if int(text) >= minimum_seats:
+            send_notification("There are " + text + " seats available!", telegram_chat_id)
 browser.close()
 
-if parterre != 0:
-    scrivi_log("Posti parterre: " + str(parterre))
-uffi = open("/fansale_tracker/log.txt", "a")
+# Log the current time just to check later that everything ran correctly
+logfile = open("./log.txt", "a")
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
-uffi.write(current_time + "\n")
-uffi.close()
+logfile.write(current_time + "\n")
+logfile.close()
